f960da5e6983bb57326326c0933b503c96b57ea3
ludoff <ludovicofiore25@gmail.com>
2024-09-10 17:04:39 +0200

    feat: add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,193 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
+<footer>
+    <!-- section con bg -->
+    <section class="icon-bg">
+        <div class="container">
+
+            <div class="col-container">
+
+                <div class="col">
+                    <div class="list">
+                        <!-- lista sopra -->
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>
+                                <a href="#">CHARACTERS</a>
+                            </li>
+                            <li>
+                                <a href="#">COMICS</a>
+                            </li>
+                            <li>
+                                <a href="#">MOVIES</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">GAMES</a>
+                            </li>
+                            <li>
+                                <a href="#">COLLECTIBLES</a>
+                            </li>
+                            <li>
+                                <a href="#">VIDEOS</a>
+                            </li>
+                            <li>
+                                <a href="#">FANS</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <!-- lista sotto prima col -->
+                    <div class="list">
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>
+                                <a href="#">CHARACTERS</a>
+                            </li>
+                            <li>
+                                <a href="#">COMICS</a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <!-- lista seconda col -->
+                    <div class="list">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>
+                                <a href="#">CHARACTERS</a>
+                            </li>
+                            <li>
+                                <a href="#">COMICS</a>
+                            </li>
+                            <li>
+                                <a href="#">MOVIES</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">GAMES</a>
+                            </li>
+                            <li>
+                                <a href="#">COLLECTIBLES</a>
+                            </li>
+                            <li>
+                                <a href="#">VIDEOS</a>
+                            </li>
+                            <li>
+                                <a href="#">FANS</a>
+                            </li>
+                            <li>
+                                <a href="#">NEWS</a>
+                            </li>
+                            <li>
+                                <a href="#">SHOP</a>
+
+                            </li>
+                            <li>
+                                <a href="#">NEWS</a>
+                            </li>
+                            <li>
+                                <a href="#">SHOP</a>
+
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <!-- lista terza col -->
+                    <div class="list">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>
+                                <a href="#">CHARACTERS</a>
+                            </li>
+                            <li>
+                                <a href="#">COMICS</a>
+                            </li>
+                            <li>
+                                <a href="#">MOVIES</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">GAMES</a>
+                            </li>
+                            <li>
+                                <a href="#">COLLECTIBLES</a>
+                            </li>
+
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+            <!-- immagine logo footer -->
+            <div class="image-container">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="logo dc footer">
+            </div>
+        </div>
+
+    </section>
+
+    <!-- section finale -->
+    <section id="final-section">
+        <div class="container">
+
+            <div class="sign-button">
+                <a href="#">
+                    SIGN-UP NOW!
+                </a>
+            </div>
+
+            <div class="social">
+                <span>FOLLOW US</span>
+
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="link facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="link twitter">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="link youtube">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="link pinterest">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="link periscope">
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+
+    </section>
 </footer>
