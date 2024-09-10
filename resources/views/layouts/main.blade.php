1cc624713eb042ddf8e53b9cea422a1667427008
ludoff <ludovicofiore25@gmail.com>
2024-09-10 16:50:52 +0200

    feat: add assets and header in page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +11,10 @@
     {{-- Includiamo gli assets con la direttiva @vite --}}
     @vite('resources/js/app.js')
 </head>
+
 <body>
 
-    @include('partials.navbar')
+    @include('partials.header')
 
     @yield('hero')
 
@@ -21,4 +23,5 @@
     @include('partials.footer')
 
 </body>
+
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,48 @@
+<header>
+    <div class="row container">
+
+        <span id="header-logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC">
+        </span>
+
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="#">COMICS</a>
+            </li>
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+            <li>
+                <a href="#">TV</a>
+            </li>
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+            <li>
+                <a href="#">FANS</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+
+            </li>
+
+        </ul>
+    </div>
+
+</header>
+
+<div id="jumbotron">
+    <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron">
+</div>
